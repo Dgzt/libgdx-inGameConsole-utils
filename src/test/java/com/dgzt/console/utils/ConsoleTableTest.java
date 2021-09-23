5262c941d9dd9093417f12443aced41194566ffa
Zsuró Tibor <zsurotibor@gmail.com>
2021-09-23 12:26:16 +0200

    Check printed text instead of print text

--- src/test/java/com/dgzt/console/utils/ConsoleTableTest.java
+++ src/test/java/com/dgzt/console/utils/ConsoleTableTest.java
@@ -1,6 +1,7 @@
 package com.dgzt.console.utils;
 
 import com.strongjoshua.console.Console;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -9,9 +10,24 @@ import org.mockito.stubbing.Answer;
 
 public class ConsoleTableTest {
 
+    private static final String TABLE_WITH_HEADER_EXPECTED_RESULT =
+            "+----------+----------+\n" +
+            "| Header 1 | Header 2 |\n" +
+            "+----------+----------+\n" +
+            "| row 11   | row 12   |\n" +
+            "| row 21   | row 22   |\n" +
+            "+----------+----------+\n";
+
+    private static final String TABLE_WITHOUT_HEADER_EXPECTED_RESULT =
+            "| row 11 | row 12              |\n" +
+            "| row 21 | row 22              |\n" +
+            "| row 21 | extra length row 22 |\n";
+
     private Console console;
 
     private ConsoleTable consoleTable;
+
+    private String printedText;
 
     @Before
     public void setup() {
@@ -20,12 +36,13 @@ public class ConsoleTableTest {
             @Override
             public Object answer(final InvocationOnMock invocation) throws Throwable {
                 final Object[] args = invocation.getArguments();
-                System.out.println(args[0]);
+                printedText += args[0] + "\n";
                 return null;
             }
         }).when(console).log(Mockito.anyString());
 
         consoleTable = new ConsoleTable();
+        printedText = "";
     }
 
     @Test
@@ -35,13 +52,18 @@ public class ConsoleTableTest {
         consoleTable.addRow("row 21", "row 22");
 
         consoleTable.print(console);
+
+        Assert.assertEquals(printedText, TABLE_WITH_HEADER_EXPECTED_RESULT);
     }
 
     @Test
     public void tableWithoutHeader() {
         consoleTable.addRow("row 11", "row 12");
         consoleTable.addRow("row 21", "row 22");
+        consoleTable.addRow("row 21", "extra length row 22");
 
         consoleTable.print(console);
+
+        Assert.assertEquals(printedText, TABLE_WITHOUT_HEADER_EXPECTED_RESULT);
     }
 }
